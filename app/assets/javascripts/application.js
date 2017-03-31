c0689cc953b581750237c68eb4f05ec3bd9a87e2
lulian <lulian778899@gmail.com>
2017-04-01 06:49:57 +0800

    add bootstrap flash function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,9 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
+
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//=require bootstrap/alert
+//= require_tree .
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
